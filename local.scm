5c60ec3d54db6ee8f16964bd88c028ecfb94e7d0
kai <kai@deathmetal>
2014-11-03 20:14:34 +0100

    .

---
+++ local.scm
@@ -0,0 +1,31 @@
+(use-modules (srfi srfi-1))
+
+(define bookmarks '())
+
+(define (bookmark model name data)
+  (set! bookmarks (cons (list model name data)
+                        bookmarks)))
+
+(define (bookmarks-for-model model)
+  (remove (lambda (x) (not (string=? (first x) model)))
+          bookmarks))
+
+(define (list-bookmarks model)
+  (map (lambda (x) (second x))
+       (bookmarks-for-model model)))
+
+(define (select-bookmark model mark)
+  (let* ((marks (bookmarks-for-model model))
+         (matching (remove (lambda (x) (not (string-contains (second x) mark)))
+                           marks)))
+    (if (not (null? matching))
+        (let ((match (first matching)))
+          (eval `(begin
+                   (change-lookat-of-cam! (current-camera) ,@(third match))
+                   (recompute-gl-matrices-of-cam (current-camera)))
+                (interaction-environment))
+          (second match))
+        #f)))
+    
+(if (file-exists? "bookmarks")
+    (primitive-load "bookmarks"))
