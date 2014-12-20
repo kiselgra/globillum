0b2ec86abe6284e23b111a593e46d3dbf41da099
kai <kai@deathmetal>
2014-12-20 14:54:07 +0100

    gralab fix.

--- sponza.lights.scm
+++ sponza.lights.scm
@@ -1,11 +1,11 @@
-;(light "area"
-;       :type :rect
-;       :color (list  20  30 80)
-;       :pos   (list 700 670  0)
-;       :dir   (list   0  -1  0)
-;       :up    (list   0   0  1)
-;       :dim   (list 100 100))
-;
+(light "area"
+       :type :rect
+       :color (list  50  50 50)
+       :pos   (list 700 670  0)
+       :dir   (list   0  -1  0)
+       :up    (list   0   0  1)
+       :dim   (list 100 100))
+
 ;(light "area2"
 ;       :type :rect
 ;       :color (list  90  70 50)
@@ -14,9 +14,9 @@
 ;       :up    (list   0   0  1)
 ;       :dim   (list 100 100))
 
-(light "sky"
-       :type :sky
-       ;:map "cgskies-0319-free.png"
-       :map "CGSkies_0056_free.exr"
-       :diameter 1000
-       :scale 20)
+;(light "sky"
+;       :type :sky
+;       ;:map "cgskies-0319-free.png"
+;       :map "CGSkies_0056_free.exr"
+;       :diameter 1000
+;       :scale 20)
