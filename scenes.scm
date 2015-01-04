8ba93f622b54389c1483ff07063ebc86d80ed84e
kai <kai@gralab>
2015-01-04 22:59:55 +0100

    bugfix: objects without material resulted in kernel crash. added fallback material and proper multi-obj support.

--- scenes.scm
+++ scenes.scm
@@ -1,8 +1,20 @@
 (use-modules (ice-9 optargs))
 (read-set! keywords 'prefix)
 
-(define* (add-model name :key type (is-base #f))
-  (cond ((eq? type :obj) (add-model% name 0 is-base))
+(define (row-maj->col-maj-str trafo)
+  (let ((e (lambda (n) (list-ref trafo n))))
+    (format #f "~a ~a ~a ~a ~a ~a ~a ~a ~a ~a ~a ~a ~a ~a ~a ~a"
+	    (e 0) (e 4) (e 8) (e 12)
+	    (e 1) (e 5) (e 9) (e 13)
+	    (e 2) (e 6) (e 10) (e 14)
+	    (e 3) (e 7) (e 11) (e 15))))
+
+(define (row-maj->row-maj-str trafo)
+  (let ((e (lambda (n) (list-ref trafo n))))
+    (format #f "~{~a~^ ~}" trafo)))
+
+(define* (add-model name :key type (is-base #f) (trafo '(1 0 0 0  0 1 0 0  0 0 1 0  0 0 0 1)))
+  (cond ((eq? type :obj) (add-model% name 0 is-base (row-maj->row-maj-str trafo)))
 	((eq? type :subd) (add-model% name 1 is-base))
 	(else (error "unknown model type: " type))))
 
