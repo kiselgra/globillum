573d242514b1a06e3b52537aff3ccbfcc33f5a92
simaprus <magdalena.prus@informatik.uni-erlangen.de>
2015-01-13 13:18:51 +0100

    materials and .obj and bookmarks

---
+++ dino.lights.scm
@@ -0,0 +1,23 @@
+(light "area"
+       :type :rect
+       :color (list  1400 840 520)
+      :pos   (list  -10.530327 330.913345 197.354675)
+       :dir   (list   0  -1  0)
+       :up    (list   0   0  1)
+       :dim   (list 1.50 1.50))
+
+;(light "area2"
+;       :type :rect
+;       :color (list  90  70 50)
+;       :pos   (list   0 100  0)
+;       :dir   (list   0  -1  0)
+;       :up    (list   0   0  1)
+;       :dim   (list 100 100))
+
+;(let ((home (getenv "HOME")))
+;  (light "sky"
+ ;        :type :sky
+;         :map "glacier.exr" 
+  ;   :map (string-append home "/render-data/images/skylight-sunset.exr")
+   ;      :diameter 1000
+     ;    :scale 0.5))
--- dinoHead.scene.scm
+++ dinoHead.scene.scm
@@ -1,10 +1,10 @@
 (let ((home (getenv "HOME")))
  (add-model "/tmp/boxes-d3.obj" :type :obj :is-base #t)
  (add-model "/share/space1/simaprus/TREX/TREX_ptx/COLOR/bc_body_trexAA.ptx"
-  :type :subd :disp "/share/space1/simaprus/TREX/TREX_ptx/DISP8/bc_body_trexAA.ptx"
-  :proxy "/tmp/trex.obj")
- (add-model "/tmp/ram/trex/COLOR/hc_upteeth_trexAA.ptx"
-  :type :subd :disp "/tmp/ram/trex/DISP8/hc_upteeth_trexAA.ptx")
+ :type :subd :disp "/share/space1/simaprus/TREX/TREX_ptx/DISP8/bc_body_trexAA.ptx")
+;  :proxy "/tmp/trex.obj")
+ (add-model "/share/space1/simaprus/TREX/TREX_ptx/COLOR/hc_upteeth_trexAA.ptx"
+  :type :subd :disp "/share/space1/simaprus/TREX/TREX_ptx/DISP8/hc_upteeth_trexAA.ptx")
  (subd-tess 3 7) ;use normal subdiv for 3 levels, quantization for 5 levels.
  (displacement-scale 3)
  (path-length 2)
